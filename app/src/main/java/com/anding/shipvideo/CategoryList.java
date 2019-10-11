d81c9f22d7b7acc77b5339dfb945bf785096c05b
liuwei <451614272@qq.com>
2019-10-11 10:28:40 +0800

    tv 初始版本

---
+++ app/src/main/java/com/anding/shipvideo/CategoryList.java
@@ -0,0 +1,93 @@
+/*
+ * Copyright (C) 2017 The Android Open Source Project
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except
+ * in compliance with the License. You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software distributed under the License
+ * is distributed on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express
+ * or implied. See the License for the specific language governing permissions and limitations under
+ * the License.
+ */
+
+package com.anding.shipvideo;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public final class CategoryList {
+    private static List<Category> list;
+    private static long count = 0;    //电影种类
+    public static final String HEADER_CATEGORYS[] = {
+            "行业",
+            "安全类别",
+            "工种",
+            "综合",
+    };
+
+    public static List<Category> getList() {
+        if (list == null) {
+            list = setupCategorys();
+        }
+        return list;
+    }
+
+    public static List<Category> setupCategorys() {
+        list = new ArrayList<>();
+        String title[] = {
+                "Zeitgeist 2010_ Year in Review",
+                "Google Demo Slam_ 20ft Search",
+                "Introducing Gmail Blue",
+                "Introducing Google Fiber to the Pole",
+                "Introducing Google Nose"
+        };
+
+        String description = "Fusce id nisi turpis. Praesent viverra bibendum semper. "
+                + "Donec tristique, orci sed semper lacinia, quam erat rhoncus massa, non congue tellus est "
+                + "quis tellus. Sed mollis orci venenatis quam scelerisque accumsan. Curabitur a massa sit "
+                + "amet mi accumsan mollis sed et magna. Vivamus sed aliquam risus. Nulla eget dolor in elit "
+                + "facilisis mattis. Ut aliquet luctus lacus. Phasellus nec commodo erat. Praesent tempus id "
+                + "lectus ac scelerisque. Maecenas pretium cursus lectus id volutpat.";
+
+        String bgImageUrl[] = {
+                "http://commondatastorage.googleapis.com/android-tv/Sample%20videos/Zeitgeist/Zeitgeist%202010_%20Year%20in%20Review/bg.jpg",
+                "http://commondatastorage.googleapis.com/android-tv/Sample%20videos/Demo%20Slam/Google%20Demo%20Slam_%2020ft%20Search/bg.jpg",
+                "http://commondatastorage.googleapis.com/android-tv/Sample%20videos/April%20Fool's%202013/Introducing%20Gmail%20Blue/bg.jpg",
+                "http://commondatastorage.googleapis.com/android-tv/Sample%20videos/April%20Fool's%202013/Introducing%20Google%20Fiber%20to%20the%20Pole/bg.jpg",
+                "http://commondatastorage.googleapis.com/android-tv/Sample%20videos/April%20Fool's%202013/Introducing%20Google%20Nose/bg.jpg",
+        };
+        String cardImageUrl[] = {
+                "http://commondatastorage.googleapis.com/android-tv/Sample%20videos/Zeitgeist/Zeitgeist%202010_%20Year%20in%20Review/card.jpg",
+                "http://commondatastorage.googleapis.com/android-tv/Sample%20videos/Demo%20Slam/Google%20Demo%20Slam_%2020ft%20Search/card.jpg",
+                "http://commondatastorage.googleapis.com/android-tv/Sample%20videos/April%20Fool's%202013/Introducing%20Gmail%20Blue/card.jpg",
+                "http://commondatastorage.googleapis.com/android-tv/Sample%20videos/April%20Fool's%202013/Introducing%20Google%20Fiber%20to%20the%20Pole/card.jpg",
+                "http://commondatastorage.googleapis.com/android-tv/Sample%20videos/April%20Fool's%202013/Introducing%20Google%20Nose/card.jpg"
+        };
+
+        for (int index = 0; index < title.length; ++index) {
+            list.add(
+                    buildCategoryInfo(
+                            title[index],
+                            description, cardImageUrl[index],
+                            bgImageUrl[index]));
+        }
+
+        return list;
+    }
+
+    private static Category buildCategoryInfo(
+            String title,
+            String description,
+            String cardImageUrl,
+            String backgroundImageUrl) {
+        Category category = new Category();
+        category.setId(count++);
+        category.setTitle(title);
+        category.setDescription(description);
+        category.setCardImageUrl(cardImageUrl);
+        category.setBackgroundImageUrl(backgroundImageUrl);
+        return category;
+    }
+}
