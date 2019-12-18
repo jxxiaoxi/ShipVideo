eca44006ad3c11e80532ef4fb5cf9e4d3d8665d0
liuwei <451614272@qq.com>
2019-12-18 21:54:09 +0800

    视频源采用腾讯视频

--- app/src/main/java/com/anding/shipvideo/activity/PlaybackActivity.java
+++ app/src/main/java/com/anding/shipvideo/activity/PlaybackActivity.java
@@ -16,19 +16,37 @@ package com.anding.shipvideo.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v4.app.FragmentActivity;
+import android.util.Log;
 import android.view.KeyEvent;
 import android.widget.Toast;
 
 //import androidx.fragment.app.FragmentActivity;
 
+import com.alibaba.fastjson.JSON;
 import com.anding.shipvideo.R;
+import com.anding.shipvideo.data.Video;
 import com.anding.shipvideo.fragment.PlaybackVideoFragment;
+import com.anding.shipvideo.manager.VideosManager;
 import com.anding.shipvideo.player.Player;
+import com.anding.shipvideo.utils.Constants;
+import com.anding.shipvideo.utils.DatabaseUtils;
+import com.anding.shipvideo.utils.HttpUtils;
 import com.anding.shipvideo.utils.LogUtils;
 import com.anding.videopalyer.Jzvd;
 import com.bumptech.glide.Glide;
 
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+
+import okhttp3.Call;
+import okhttp3.Response;
 
 import static com.anding.videopalyer.Jzvd.STATE_AUTO_COMPLETE;
 import static com.anding.videopalyer.Jzvd.STATE_NORMAL;
@@ -40,8 +58,92 @@ import static com.anding.videopalyer.Jzvd.STATE_PREPARED;
  * Loads {@link PlaybackVideoFragment}.
  */
 public class PlaybackActivity extends FragmentActivity {
+    private static final int PLAY_VIDEO = 2;
     private Player mJzvdStd;
     public static final long SEEK_TIME = 20000;//快进退的间隔时间
+    public boolean isChangeUrl = true;
+    public static final String TAG = "PlaybackVideo";
+    public static final int REQUEST_KEY = 1;
+    String URI = "http://vv.video.qq.com/getinfo?vids=d3032vbxllc&platform=101001&charge=0&otype=json";
+    String URIKEY = "http://vv.video.qq.com/getkey?format=2&otype=json&vt=150&vid=d3032vbxllc&ran=0\\%2E9477521511726081\\\\&charge=0&filename=d3032vbxllc.mp4&platform=11";
+
+    public String mPicUrl = null;
+    public String mTitle = null;
+    //创建一个Handler
+    private Handler handler = new Handler() {
+
+        public void handleMessage(Message msg) {
+            switch (msg.what) {
+                case REQUEST_KEY:
+                    //在这里可以进行UI操作
+                    //对msg.obj进行String强制转换
+                    String uri = (String) msg.obj;
+                    requestKey(uri);
+                    break;
+                case PLAY_VIDEO:
+                    String realUri = (String) msg.obj;
+                    if (realUri != null && mPicUrl != null && mTitle != null) {
+                        mJzvdStd.setUp(realUri
+                                , "不用钱的视频");
+                        Glide.with(PlaybackActivity.this).load(mPicUrl).into(mJzvdStd.thumbImageView);
+                        mJzvdStd.startVideo();
+                    }
+                break;
+                default:
+                    break;
+            }
+        }
+
+    };
+
+    private void requestKey(String videouri) {
+        if (videouri == null) {
+            return;
+        }
+        HttpUtils.getInstance().getDataAsyn(URIKEY, new HttpUtils.NetWorkCallBack() {
+
+            @Override
+            public void success(Call call, Response response) throws IOException {
+                if (response == null) {
+                    LogUtils.d(TAG, "initVideosFromRemote response is null ");
+                    return;
+                }
+                // 2 解析JSON数据
+                String strResponse = null;
+                strResponse = response.body().string();
+                strResponse = strResponse.substring(0, strResponse.length() - 1);
+                strResponse = strResponse.replace("QZOutputJson=", "");
+                LogUtils.d(TAG, "requestKey ===>" + strResponse);
+                JSONObject jsonResp = null;
+                String key = null;
+                String filename = null;
+                String realUri = null;
+                try {
+                    jsonResp = new JSONObject(strResponse);
+                    if (jsonResp.has("key")) {
+                        key = (String) jsonResp.get("key");
+                    }
+                    if (jsonResp.has("filename")) {
+                        filename = (String) jsonResp.get("filename");
+                    }
+                    realUri = videouri + filename + "?vkey=" + key;
+                    LogUtils.d(TAG, "realUri ===>" + realUri);
+                    Message message = new Message();
+                    message.what = PLAY_VIDEO;
+                    message.obj = realUri;
+                    handler.sendMessage(message);
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+
+            @Override
+            public void failed(Call call, IOException e) {
+
+            }
+        });
+    }
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -53,25 +155,82 @@ public class PlaybackActivity extends FragmentActivity {
 //                    .replace(android.R.id.content, new PlaybackVideoFragment())
 //                    .commit();
 //        }
-        mJzvdStd =  findViewById(R.id.jz_video);
+        mJzvdStd = findViewById(R.id.jz_video);
         Intent intent = getIntent();
         if (intent == null) {
+            LogUtils.d(TAG,"--------------play type 1-------------");
             String title = intent.getStringExtra("title");
             String uri = intent.getStringExtra("uri");
             String pic = intent.getStringExtra("pic");
-            LogUtils.d("liuwei", "title : " + title + "  ;uri:  " + uri + "  ;pic : " + pic);
             mJzvdStd.setUp(uri
                     , title);
             Glide.with(this).load(pic).into(mJzvdStd.thumbImageView);
             mJzvdStd.startVideo();
             //mJzvdStd.gotoScreenFullscreen();+
+        } else if (isChangeUrl) {
+            LogUtils.d(TAG,"--------------play type 2-------------");
+            mTitle = intent.getStringExtra("title");
+            mPicUrl = intent.getStringExtra("uri");
+            changeUrl();
         } else {
+            LogUtils.d(TAG,"--------------play type 3-------------");
             mJzvdStd.setUp("\n" +
                             "http://180.153.119.11/vhot2.qqvideo.tc.qq.com/AbhcsTY0HQmqakmg8DXC8OXiSLntF9vObyZCmauthmt8/uwMROfz2r5zAoaQXGdGnC2dfhzkrvqBxjTHFA2hC4m2Yr1g7/d3032vbxllc.mp4?vkey=86741CA0D63DBD2F3BE0F0EB4E51F5C79CBBA8199949247C3FE3B730922D463A7D0A498090C3A885770A123D9A1A0B056C65E80D648E0CDD1DA0B9FDEFD4A489B87998E26D11F37AC3F85374F83F19B0B14DDBF6CB65EEED28E0A96562F437516192AB1436DAD3C578B4762488F3B450"
                     , "饺子闭眼睛");
             Glide.with(this).load("http://jzvd-pic.nathen.cn/jzvd-pic/1bb2ebbe-140d-4e2e-abd2-9e7e564f71ac.png").into(mJzvdStd.thumbImageView);
         }
         // jzvdStd.thumbImageView.setImage("http://p.qpic.cn/videoyun/0/2449_43b6f696980311e59ed467f22794e792_1/640");
+    }
+
+    private void changeUrl() {
+        HttpUtils.getInstance().getDataAsyn(URI, new HttpUtils.NetWorkCallBack() {
+            @Override
+            public void success(Call call, Response response) throws IOException {
+                if (response == null) {
+                    LogUtils.d(TAG, "initVideosFromRemote response is null ");
+                    return;
+                }
+                // 2 解析JSON数据
+                String strResponse = null;
+                strResponse = response.body().string();
+                if (strResponse == null) {
+                    return;
+                }
+                LogUtils.d(TAG, "start success==> " + strResponse);
+                strResponse = strResponse.substring(0, strResponse.length() - 1);
+                strResponse = strResponse.replace("QZOutputJson=", "");
+                LogUtils.d(TAG, " success==> " + strResponse);
+                try {
+                    JSONObject jsonResp = new JSONObject(strResponse);
+                    if (jsonResp.has("vl")) {
+                        JSONObject vl = (JSONObject) jsonResp.get("vl");
+                        JSONArray kk = (JSONArray) vl.get("vi");
+                        if (kk.length() > 0) {
+                            JSONObject jj = kk.getJSONObject(0);
+                            JSONObject gg = jj.getJSONObject("ul");
+                            JSONArray dd = (JSONArray) gg.get("ui");
+                            if (dd.length() > 0) {
+                                JSONObject mm = (JSONObject) dd.get(0);
+                                String url = (String) mm.get("url");
+                                LogUtils.d(TAG, " success== url ===> " + url);
+                                Message message = new Message();
+                                message.what = REQUEST_KEY;
+                                message.obj = url;
+                                handler.sendMessage(message);
+                            }
+                        }
+                    }
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+            }
+
+            @Override
+            public void failed(Call call, IOException e) {
+                LogUtils.d(TAG, "initVideosFromRemote failed " + e.toString());
+            }
+        });
+
     }
 
     @Override
